3f2afcd9cade0424dd3d25f0a9a5659701d40ce3
Mithun Param <paramatm@mcmaster.ca>
2024-07-25 00:36:23 -0400

    setting up authentication popup form

--- src/components/AuthPopup/AuthPopup.tsx
+++ src/components/AuthPopup/AuthPopup.tsx
@@ -3,10 +3,12 @@ import './AuthPopup.css'
 import Image from 'next/image'
 import logo from '@/assets/gymbuddy-logo.png'
 import Input from '@mui/joy/Input';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
 
 
 const AuthPopup = () => {
-    const [showLogin, setShowLogin] = useState<boolean>(false)
+    const [showSignUp, setShowSignUp] = useState<boolean>(false)
 
     const handleLogin = () => {}
     const handleSignUp = () => {}
@@ -14,32 +16,53 @@ const AuthPopup = () => {
   return (
     <div className='auth'>
         {
-            showLogin ? (
-                <div className='signup-form'>
-                <div className='logo'>
-                    <Image src={logo} alt="App logo"/>
-                </div>
-                <div className='signup-details'>
-                    <h1>Sign Up</h1>
-                    <form>
-                        <Input color="success" disabled={false} placeholder="Email" size="md" variant="outlined"/>
-                        <Input color="success" disabled={false} placeholder="Password" size="md" variant="outlined"/>
-                    </form>
-                    <span>Not a member yet? <button onClick={() => {handleSignUp()}}>Sign Up</button></span>
-                </div>
-            </div>
-            ) : (
-                <div className='signup-form'>
+            showSignUp ? (
+                <div className='auth-form'>
                     <div className='logo'>
                         <Image src={logo} alt="App logo"/>
                     </div>
                     <div className='signup-details'>
                         <h1>Sign Up</h1>
                         <form>
-                            <Input color="success" disabled={false} placeholder="Email" size="md" variant="outlined"/>
-                            <Input color="success" disabled={false} placeholder="Password" size="md" variant="outlined"/>
+                            <Input color="success" placeholder="Email" size="md" variant="outlined"/>
+                            <Input color="success" placeholder="Password" size="md" variant="outlined" type="password"/>
+                        
+                            <div className='left-right_inputs'>
+                                <Input color="success" placeholder="Age" size="md" variant="outlined" type="number"/>
+                                <Input color="success" placeholder="Weight" size="md" variant="outlined" type="number"/>
+                            </div>
+
+                            <span>Height</span>
+                            <div className="left-right_inputs">
+                                <Input color="success" placeholder="ft" size="md" variant="outlined" type="number"/>
+                                <Input color="success" placeholder="in" size="md" variant="outlined" type="number"/>
+                            </div>
+
+                            <Select color="success" placeholder="Gender" size="md">
+                                <Option value="male">Male</Option>
+                                <Option value="female">Female</Option>
+                                <Option value="non-binary">Non-binary</Option>
+                                <Option value="other">Other</Option>
+                            </Select>
+
+                            <button onClick={() => {handleSignUp()}}>Sign Up</button>
                         </form>
-                        <span>Not a member yet? <button onClick={() => {handleSignUp()}}>Sign Up</button></span>
+                        <span>Already a member? <span className='underlined-text' onClick={() => {setShowSignUp(false)}}>Login</span></span>
+                    </div>
+                </div>
+            ) : (
+                <div className='auth-form'>
+                    <div className='logo'>
+                        <Image src={logo} alt="App logo"/>
+                    </div>
+                    <div className='signup-details'>
+                        <h1>Login</h1>
+                        <form>
+                            <Input color="success" placeholder="Email" size="md" variant="outlined"/>
+                            <Input color="success" placeholder="Password" size="md" variant="outlined" type="password"/>
+                            <button onClick={() => {handleLogin()}}>Login</button>
+                        </form>
+                        <span>Not a member yet? <span className='underlined-text' onClick={() => {setShowSignUp(true)}}>Sign Up</span></span>
                     </div>
                 </div>
             )
