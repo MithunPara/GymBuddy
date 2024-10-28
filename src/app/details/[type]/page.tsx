34a02ad5de789670a8125d34d847beef4964650f
MithunPara <paramatm@mcmaster.ca>
2024-10-28 17:02:17 -0400

    designed details page popup (in progress)

--- src/app/details/[type]/page.tsx
+++ src/app/details/[type]/page.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import './DetailsPage.css'
 import CalorieIntakePopup from '@/components/DetailsFormPopup/CalorieIntake/CalorieIntakePopup'
 import { LineChart } from '@mui/x-charts'
+import { FaEdit } from "react-icons/fa";
 
 const page = () => {
     const chartParameters = {
@@ -10,6 +11,7 @@ const page = () => {
     }
 
     const [dataG1, setDataG1] = useState<any>(null)
+    const [calorieIntakePopup, setCalorieIntakePopup] = useState<boolean>(false)
 
     const getDataG1 = async () => {
         let data = [
@@ -51,14 +53,14 @@ const page = () => {
         ]
 
         let chartValues = data.map((item: any) => {
-            let val = JSON.stringify(item.value)
+            let val = JSON.stringify(item.value) // must convert values to string to display using LineChart component
             return val
         })   
 
         let xAxisData = data.map((item: any) => {
             let val = new Date(item.date)
             return val
-        })
+        })       
 
         setDataG1({
             title: 'Calorie Intake',
@@ -78,27 +80,106 @@ const page = () => {
     }, [])
 
   return (
-    <div>
+    <div className='details-page'>
+        <div className="chart-section">
+            {
+                dataG1 && 
+                <LineChart
+                    xAxis={[{ 
+                        id: 'Day',
+                        data: dataG1.xAxis.data,
+                        label: dataG1.xAxis.label,
+                        scaleType: dataG1.xAxis.scaleType,
+                        valueFormatter: (date: any) => {
+                            return date.getDate().toString()
+                        }
+                    }]}
+                    series={[
+                        {
+                            data: dataG1.data,
+                            label: dataG1.title
+                        },
+                    ]}
+                    {...chartParameters}
+                />
+            }
+        </div>
+        <div className="chart-section">
+            {
+                dataG1 && 
+                <LineChart
+                    xAxis={[{ 
+                        id: 'Day',
+                        data: dataG1.xAxis.data,
+                        label: dataG1.xAxis.label,
+                        scaleType: dataG1.xAxis.scaleType,
+                        valueFormatter: (date: any) => {
+                            return date.getDate().toString()
+                        }
+                    }]}
+                    series={[
+                        {
+                            data: dataG1.data,
+                            label: dataG1.title
+                        },
+                    ]}
+                    {...chartParameters}
+                />
+            }
+        </div>
+        <div className="chart-section">
+            {
+                dataG1 && 
+                <LineChart
+                    xAxis={[{ 
+                        id: 'Day',
+                        data: dataG1.xAxis.data,
+                        label: dataG1.xAxis.label,
+                        scaleType: dataG1.xAxis.scaleType,
+                        valueFormatter: (date: any) => {
+                            return date.getDate().toString()
+                        }
+                    }]}
+                    series={[
+                        {
+                            data: dataG1.data,
+                            label: dataG1.title
+                        },
+                    ]}
+                    {...chartParameters}
+                />
+            }
+        </div>
+        <div className="chart-section">
+            {
+                dataG1 && 
+                <LineChart
+                    xAxis={[{ 
+                        id: 'Day',
+                        data: dataG1.xAxis.data,
+                        label: dataG1.xAxis.label,
+                        scaleType: dataG1.xAxis.scaleType,
+                        valueFormatter: (date: any) => {
+                            return date.getDate().toString()
+                        }
+                    }]}
+                    series={[
+                        {
+                            data: dataG1.data,
+                            label: dataG1.title
+                        },
+                    ]}
+                    {...chartParameters}
+                />
+            }
+        </div>
+        <button className='edit-button' onClick={() => {setCalorieIntakePopup(true)}}>
+            <FaEdit />
+        </button>
+
         {
-            dataG1 && 
-            <LineChart
-                xAxis={[{ 
-                    id: 'Day',
-                    data: dataG1.xAxis.data,
-                    label: dataG1.xAxis.label,
-                    scaleType: dataG1.xAxis.scaleType,
-                    // valueFormatter: (date: any) => {
-                    //     return date.getDate();
-                    // }
-                }]}
-                series={[
-                    {
-                        data: dataG1.data,
-                        label: dataG1.title
-                    },
-                ]}
-                {...chartParameters}
-            />
+            // display popup if edit button is pressed
+            calorieIntakePopup && <CalorieIntakePopup setCalorieIntakePopup={setCalorieIntakePopup} /> 
         }
     </div>
   )
--- src/components/DetailsFormPopup/CalorieIntake/CalorieIntakePopup.tsx
+++ src/components/DetailsFormPopup/CalorieIntake/CalorieIntakePopup.tsx
@@ -1,8 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../popup.css'
+import dayjs, { Dayjs } from 'dayjs';
+import { TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+import { Datepicker, DatepickerEvent} from "@meinefinsternis/react-horizontal-date-picker";
+import { enCA } from "date-fns/locale";
+import { MdDelete, MdClose } from "react-icons/md";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DigitalClock } from '@mui/x-date-pickers/DigitalClock';
 
-const CalorieIntakePopup = () => {
+interface CalorieIntakePopupProps {
+  setCalorieIntakePopup: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const CalorieIntakePopup: React.FC<CalorieIntakePopupProps> = ({ setCalorieIntakePopup }) => {
+
+  const addDays = (date: any, days: number) => {
+    console.log(date, days, "original date");
+    date.setDate(date.getDate() + days);
+    console.log(date, "new date");
+    return date;
+  }
+
+  const [date, setDate] = useState<{
+    endValue: Date | null;
+    startValue: Date | null;
+    // rangeDates: Date[] | null;
+  }>({
+    startValue: new Date(),
+    endValue: addDays(new Date(), 100),
+    // rangeDates: [],
+  });
+
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17T15:30'));
+
+  const handleChange = (d: DatepickerEvent) => {
+    const [startValue, endValue] = d;
+    setDate((prev) => ({ ...prev, endValue, startValue }));
+  };
+
   return (
-    <div>CalorieIntakePopup</div>
+    <div className='popup'>
+      <div className='popup-form'>
+        <button className='close' onClick={() => {setCalorieIntakePopup(false)}}>
+          <MdClose />
+        </button>
+        <Datepicker
+          onChange={handleChange}
+          locale={enCA}
+          startValue={date.startValue}
+          endValue={date.endValue}
+          // classNames={{ dayLabel: "hello" }}
+        />
+
+        <TextField id="outlined-basic" label="Food item" variant="outlined" color="success" />
+        <TextField id="outlined-basic" label="Food item amount (g)" variant="outlined" color="success" />
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DigitalClock 
+            sx={{ // Override some of the styling options of the MUI DigitalClock component (different background colour and add rounded borders)
+              '.Mui-selected': {
+                backgroundColor: '#007500',
+                color: 'white',
+                borderRadius: '5px',
+                '&:focus-visible, &:hover': {
+                  backgroundColor: '#006000',
+                  borderRadius: '5px',
+                },
+              },
+            }} className='clock-style' value={value} onChange={(newValue) => setValue(newValue)}/>
+        </LocalizationProvider> 
+
+        <Button variant="contained" color="success">
+          Save
+        </Button>
+
+        <div className='break-line'>
+          <div className='daily-food-items'>
+            <div className='food-item'>
+              <h3>Chicken Breast</h3>
+              <h3>150 g</h3>
+              <button><MdDelete/></button>
+            </div>
+            <div className='food-item'>
+              <h3>Avocado</h3>
+              <h3>100 g</h3>
+              <button><MdDelete/></button>
+            </div>
+            <div className='food-item'>
+              <h3>White Rice</h3>
+              <h3>110 g</h3>
+              <button><MdDelete/></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
