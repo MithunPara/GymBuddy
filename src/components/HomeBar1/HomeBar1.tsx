bc499233827f530cc6655e5adf8ae92cd105714a
MithunPara <paramatm@mcmaster.ca>
2025-01-14 16:39:05 -0500

    added functionality to retrieve user tracking/goals from backend

--- src/components/HomeBar1/HomeBar1.tsx
+++ src/components/HomeBar1/HomeBar1.tsx
@@ -7,51 +7,67 @@ const HomeBar1 = () => {
     const [progress, setProgress] = useState<any>(null)
 
     const getProgress = async () => {
-        let temp: any = [
-            {
-              "name": "Calories Intake",
-              "value": 1000,
-              "unit": "cal",
-              "target": 2500,
-              "targetUnit": "cal"
-            },
-            {
-              "name": "Sleep",
-              "value": 2,
-              "unit": "hrs",
-              "target": 8,
-              "targetUnit": "hrs"
-            },
-            {
-              "name": "Steps",
-              "value": 100,
-              "unit": "steps",
-              "target": 10000,
-              "targetUnit": "steps"
-            },
-            {
-              "name": "Water",
-              "value": 1000,
-              "unit": "ml",
-              "target": 3000,
-              "targetUnit": "ml"
-            },
-            {
-              "name": "Weight",
-              "value": 80,
-              "unit": "kg",
-              "target": 70,
-              "targetUnit": "kg"
-            },
-            {
-              "name": "Workout",
-              "value": 4,
-              "unit": "days",
-              "target": 6,
-              "targetUnit": "days"
-            }
-        ]
-        setProgress(temp)
+        // let temp: any = [
+        //     {
+        //       "name": "Calories Intake",
+        //       "value": 1000,
+        //       "unit": "cal",
+        //       "target": 2500,
+        //       "targetUnit": "cal"
+        //     },
+        //     {
+        //       "name": "Sleep",
+        //       "value": 2,
+        //       "unit": "hrs",
+        //       "target": 8,
+        //       "targetUnit": "hrs"
+        //     },
+        //     {
+        //       "name": "Steps",
+        //       "value": 100,
+        //       "unit": "steps",
+        //       "target": 10000,
+        //       "targetUnit": "steps"
+        //     },
+        //     {
+        //       "name": "Water",
+        //       "value": 1000,
+        //       "unit": "ml",
+        //       "target": 3000,
+        //       "targetUnit": "ml"
+        //     },
+        //     {
+        //       "name": "Weight",
+        //       "value": 80,
+        //       "unit": "kg",
+        //       "target": 70,
+        //       "targetUnit": "kg"
+        //     },
+        //     {
+        //       "name": "Workout",
+        //       "value": 4,
+        //       "unit": "days",
+        //       "target": 6,
+        //       "targetUnit": "days"
+        //     }
+        // ]
+        // setProgress(temp)
+        fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/datareport/getreport', {
+          method: 'GET',
+          credentials: 'include'
+        })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          if (data.ok) {
+            setProgress(data.data)
+          } else {
+            setProgress([])
+          }
+        }).catch(err => {
+          console.log(err)
+          setProgress([])
+        })
     }
 
     useEffect(() => { // whenever this component is rendered, we want to get the updated data
@@ -67,11 +83,11 @@ const HomeBar1 = () => {
                     <div className='card-header'>
                         <div className='card-header-section'>
                             <div className='card-header-name'>{item.name}</div>
-                            <div className='card-header-value'>{item.value} {item.unit}</div>
+                            <div className='card-header-value'>{parseInt(item.value)} {item.unit}</div>
                         </div>
                         <div className='card-header-section'>
                             <div className='card-header-name'>Target</div>
-                            <div className='card-header-value'>{item.target} {item.targetUnit}</div>
+                            <div className='card-header-value'>{parseInt(item.target)} {item.targetUnit}</div>
                         </div>
                     </div>
                     <CircularProgress color="neutral" determinate size="md" variant="solid" value={Math.min((item.value / item.target) * 100, 100)}/>
