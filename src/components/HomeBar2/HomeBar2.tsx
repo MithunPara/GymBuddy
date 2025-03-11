48442b9ad043be85eaa50258fc261e7cc3ad8b52
MithunPara <paramatm@mcmaster.ca>
2025-03-11 16:36:35 -0400

    frontend can now retrieve all workout routines from backend and display the exercises along with exercise descriptions

--- src/components/HomeBar2/HomeBar2.tsx
+++ src/components/HomeBar2/HomeBar2.tsx
@@ -15,34 +15,49 @@ const HomeBar2 = () => {
     const [routines, setRoutines] = useState<any[] | null>(null) // routine can be an array made up of any data type, it can be null as well
 
     const getRoutines = async () => {
-        let data: any = [
-            {
-                type: 'Chest',
-                imageUrl: 'https://images.pexels.com/photos/3837757/pexels-photo-3837757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                workoutDuration: 45
-            },
-            {
-                type: 'Back',
-                imageUrl: 'https://images.pexels.com/photos/1865131/pexels-photo-1865131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                workoutDuration: 60
-            },
-            {
-                type: 'Abs',
-                imageUrl: 'https://images.pexels.com/photos/12895269/pexels-photo-12895269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-                workoutDuration: 30
-            },
-            {
-                type: 'Shoulders',
-                imageUrl: 'https://images.pexels.com/photos/6550853/pexels-photo-6550853.jpeg?auto=compress&cs=tinysrgb&w=400',
-                workoutDuration: 30
-            },
-            {
-                type: 'Arms',
-                imageUrl: 'https://images.pexels.com/photos/6550871/pexels-photo-6550871.jpeg?auto=compress&cs=tinysrgb&w=400',
-                workoutDuration: 30
+        fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/workoutroutines/getroutines', {
+            method: 'GET',
+            credentials: 'include'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.ok) {
+                setRoutines(data.data);
+            } else {
+                setRoutines([])
             }
-        ]  
-        setRoutines(data)
+        })
+        .catch(err => {
+            console.log(err);
+            setRoutines([]);
+        })
+        // let data: any = [
+        //     {
+        //         type: 'Chest',
+        //         imageUrl: 'https://images.pexels.com/photos/3837757/pexels-photo-3837757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        //         workoutDuration: 45
+        //     },
+        //     {
+        //         type: 'Back',
+        //         imageUrl: 'https://images.pexels.com/photos/1865131/pexels-photo-1865131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        //         workoutDuration: 60
+        //     },
+        //     {
+        //         type: 'Abs',
+        //         imageUrl: 'https://images.pexels.com/photos/12895269/pexels-photo-12895269.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        //         workoutDuration: 30
+        //     },
+        //     {
+        //         type: 'Shoulders',
+        //         imageUrl: 'https://images.pexels.com/photos/6550853/pexels-photo-6550853.jpeg?auto=compress&cs=tinysrgb&w=400',
+        //         workoutDuration: 30
+        //     },
+        //     {
+        //         type: 'Arms',
+        //         imageUrl: 'https://images.pexels.com/photos/6550871/pexels-photo-6550871.jpeg?auto=compress&cs=tinysrgb&w=400',
+        //         workoutDuration: 30
+        //     }
+        // ]  
     }
 
     useEffect(() => {
@@ -50,48 +65,53 @@ const HomeBar2 = () => {
     }, []) 
 
   return (
-    <div>
-        <h1 className='header1'>Routines</h1>
-        <Swiper
-        slidesPerView={1}
-        spaceBetween={10}
-        pagination={{
-        clickable: true,
-        }}
-        breakpoints={{
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-        },
-        1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-        },
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-        >
-        {
-            routines && routines.map((item: any, index: number) => {
-                return (
-                    <SwiperSlide key={index}>
-                        <div className='swiper-slide' style={{ backgroundImage: `url(${item.imageUrl})` }}
-                        onClick={() => { window.location.href = `/workouts/${item.type}` }}>
-                            <div className='swiper-slide-body'>
-                                <h2>{item.type}</h2>
-                                <span>{item.workoutDuration} minutes</span>
+    <>
+    {
+        routines &&
+        <div>
+            <h1 className='header1'>Routines</h1>
+            <Swiper
+            slidesPerView={1}
+            spaceBetween={10}
+            pagination={{
+            clickable: true,
+            }}
+            breakpoints={{
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 50,
+            },
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+            >
+            {
+                routines && routines.map((item: any, index: number) => {
+                    return (
+                        <SwiperSlide key={index}>
+                            <div className='swiper-slide' style={{ backgroundImage: `url(${item.imageURL})` }}
+                            onClick={() => { window.location.href = `/workouts?id=${item._id}` }}>
+                                <div className='swiper-slide-body'>
+                                    <h2>{item.name}</h2>
+                                    <span>{item.lengthMinutes} minutes</span>
+                                </div>
                             </div>
-                        </div>
-                    </SwiperSlide>
-                )
-            })
-        }
-        </Swiper>
-    </div>
+                        </SwiperSlide>
+                    )
+                })
+            }
+            </Swiper>
+        </div> 
+    }
+    </>
   )
 }
 
